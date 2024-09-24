5517db2e8043639ae279256bf4a641b9a13018c0
derekouyang <30915437+derekouyang@users.noreply.github.com>
2024-09-24 12:24:30 -0700

    updated tables to 2022, updated functions to use left joins

---
+++ bisg_functions.R
@@ -0,0 +1,178 @@
+library(tidyverse)
+library(doParallel)
+
+USE_CACHED <- T
+DIR_PATH <- "" # set your path
+
+geo_race_table_lst <- readRDS(paste0(DIR_PATH,"geo_race_table.rds"))
+
+p_race_given_surname <- readRDS(paste0(DIR_PATH,"p_race_given_surname.rds"))
+p_surname_given_race <- readRDS(paste0(DIR_PATH,"p_surname_given_race.rds"))
+p_race_given_firstname <- readRDS(paste0(DIR_PATH,"p_race_given_firstname.rds"))
+p_firstname_given_race <- readRDS(paste0(DIR_PATH,"p_firstname_given_race.rds"))
+
+#df is your dataframe. It just needs the fields "id" (any kind of unique identifier), "surname", "firstname" (if you're running BIFSG), and at least one of the following geo fields: "cbg", "tract", "zip", "county" (default), or "state" (specify which geo level you want to use as a parameter in the function call). The function will add columns with posterior probabilities to df.
+predict_race <- function(df, firstname=F, geo = "county"){
+  
+  race_var <- p_race_given_surname %>% select(-name) %>% names()
+  
+  if(geo == "cbg"){
+    p_race_given_geo <- geo_race_table_lst$p_race_given_cbg %>% 
+      bind_rows(geo_race_table_lst$p_race_given_us)
+    p_geo_given_race <- geo_race_table_lst$p_cbg_given_race %>% 
+      bind_rows(geo_race_table_lst$p_us_given_race)
+    
+    df <- df %>% 
+      mutate(
+        GEOID = ifelse(
+          cbg %in% p_geo_given_race$GEOID,
+          cbg,
+          "1"
+        )
+      )
+  }
+  if(geo == "tract"){
+    p_race_given_geo <- geo_race_table_lst$p_race_given_tract %>% 
+      bind_rows(geo_race_table_lst$p_race_given_us)
+    p_geo_given_race <- geo_race_table_lst$p_tract_given_race %>% 
+      bind_rows(geo_race_table_lst$p_us_given_race)
+    
+    df <- df %>% 
+      mutate(
+        GEOID = ifelse(
+          tract %in% p_geo_given_race$GEOID,
+          tract,
+          "1"
+        )
+      )
+  }
+  if(geo == "zip"){
+    p_race_given_geo <- geo_race_table_lst$p_race_given_zcta %>% 
+      bind_rows(geo_race_table_lst$p_race_given_us)
+    p_geo_given_race <- geo_race_table_lst$p_zcta_given_race %>% 
+      bind_rows(geo_race_table_lst$p_us_given_race)
+    
+    df <- df %>% 
+      mutate(
+        GEOID = ifelse(
+          zip %in% p_geo_given_race$GEOID,
+          zip,
+          "1"
+        )
+      )
+  }
+  if(geo == "county"){
+    p_race_given_geo <- geo_race_table_lst$p_race_given_county %>% 
+      bind_rows(geo_race_table_lst$p_race_given_us)
+    p_geo_given_race <- geo_race_table_lst$p_county_given_race %>% 
+      bind_rows(geo_race_table_lst$p_us_given_race)
+    
+    df <- df %>% 
+      mutate(
+        GEOID = ifelse(
+          county %in% p_geo_given_race$GEOID,
+          county,
+          "1"
+        )
+      )
+  }
+  if(geo == "state"){
+    p_race_given_geo <- geo_race_table_lst$p_race_given_state %>% 
+      bind_rows(geo_race_table_lst$p_race_given_us)
+    p_geo_given_race <- geo_race_table_lst$p_state_given_race %>% 
+      bind_rows(geo_race_table_lst$p_us_given_race)
+    
+    df <- df %>% 
+      mutate(
+        GEOID = ifelse(
+          state %in% p_geo_given_race$GEOID,
+          state,
+          "1"
+        )
+      )
+  }
+  
+  df1 <- df %>%
+    inner_join(p_race_given_surname, by=c("surname"="name"))
+  
+  df1_posteriors <- NULL
+  
+  if (nrow(df1) > 0){
+    priors <- df1 %>%
+      select(all_of(race_var))
+    
+    update <- df1 %>% 
+      select(GEOID) %>% 
+      left_join(p_geo_given_race, by="GEOID") %>%
+      select(all_of(race_var))
+    post <- priors * update
+    
+    df1_posteriors <- cbind(
+      df1 %>% select(id),
+      post
+    ) 
+  }
+  
+  # step 2
+  df2 <- df %>%
+    anti_join(p_race_given_surname, by=c("surname"="name"))
+  
+  df2_posteriors <- NULL
+  if (nrow(df2) > 0){
+    priors <- df2 %>%
+      select(GEOID) %>% 
+      inner_join(p_race_given_geo, by="GEOID") %>%
+      select(all_of(race_var))
+    
+    df2_posteriors <- cbind(
+      df2 %>% select(id),
+      priors
+    )
+  }
+  
+  post_combined <- rbind(df1_posteriors, df2_posteriors) 
+  
+  if (firstname){
+    
+    firstname_update <- df %>%
+      left_join(p_firstname_given_race,
+                by=c("firstname"="name")) %>%
+      distinct() %>% 
+      inner_join(post_combined %>% select(id), ., 
+                 by="id") %>% 
+      select(all_of(race_var)) %>%
+      mutate(
+        across(everything(), ~replace_na(.,1))
+      )
+    
+    total_prior <- select(post_combined, all_of(race_var))
+    
+    post_firstname <- total_prior * firstname_update
+    post_firstname_normalized <- post_firstname / rowSums(post_firstname)
+    
+    post_combined <- cbind(post_combined %>% select(id),
+                           post_firstname_normalized)
+  }
+  
+  df_posteriors <- post_combined %>%
+    mutate(sum_to_adjust = rowSums(select(., -c(id)))) %>%
+    mutate(across(-c(id,-sum_to_adjust), ~./sum_to_adjust)) %>%
+    select(-sum_to_adjust) %>% 
+    left_join(df,., by = "id")
+  
+  return(df_posteriors)
+}
+
+# Example
+data <- data.frame(
+  id = c(1:3),
+  firstname = c("JOHN","JOSE","JIANG"),
+  surname = c("WALKER","LOPEZ","LI"),
+  cbg = c("010010201001","010010201002",NA)
+)
+
+result_bisg <- data %>% 
+  predict_race(geo = "cbg")
+
+result_bifsg <- data %>% 
+  predict_race(firstname = T, geo = "cbg")
--- create_tables.R
+++ create_tables.R
@@ -1,12 +1,14 @@
 library(tidyverse)
 library(censusapi)
 library(readxl)
+library(furrr)
 Sys.setenv(CENSUS_KEY="151c9bb8923eb37021ddfe74612864f3e8fed5c2")
-LAPLACE <- 0.001
-USE_CACHED <- Sys.getenv("USE_CACHED") == "TRUE"
-DIR_PATH <- Sys.getenv("DIR_PATH")
 
-print(paste0(DIR_PATH,"p_race_given_surname.rds"))
+USE_CACHED <- T
+DIR_PATH <- "" # set your path
+
+options(tigris_use_cache = FALSE)
+census_year <- 2022
 
 # Surname and Race
 if (!file.exists(paste0(DIR_PATH,"p_race_given_surname.rds")) | !file.exists(paste0(DIR_PATH,"p_surname_given_race.rds")) | !USE_CACHED) {
@@ -32,185 +34,44 @@ if (!file.exists(paste0(DIR_PATH,"p_race_given_surname.rds")) | !file.exists(pas
   race_surname_counts <- surnames %>%
     filter(!is.na(NAME)) %>% 
     transmute(
-      NAME = NAME,
+      name = NAME,
       COUNT = COUNT,
-      AIAN = PCTAIAN * COUNT / 100 + LAPLACE,
-      API = PCTAPI * COUNT / 100 + LAPLACE,
-      BLACK = PCTBLACK * COUNT / 100 + LAPLACE,
-      HISP = PCTHISPANIC * COUNT / 100 + LAPLACE,
-      WHITE = PCTWHITE * COUNT / 100 + LAPLACE,
-      OTHER = COUNT + 6 * LAPLACE - (AIAN + API + BLACK + HISP + WHITE)
-    ) %>% 
-    mutate (
-      COUNT = COUNT + 6 * LAPLACE
+      aian = PCTAIAN * COUNT / 100,
+      api = PCTAPI * COUNT / 100,
+      black = PCTBLACK * COUNT / 100,
+      hispanic = PCTHISPANIC * COUNT / 100,
+      white = PCTWHITE * COUNT / 100,
+      other = COUNT - (aian + api + black + hispanic + white)
     )
-  race_surname_counts %>% head() %>% print()
   
   p_race_given_surname <- race_surname_counts %>% 
     transmute(
-      NAME = NAME,
-      AIAN = AIAN / COUNT,
-      API = API / COUNT,
-      BLACK = BLACK / COUNT,
-      HISP = HISP / COUNT,
-      WHITE = WHITE / COUNT,
-      OTHER = OTHER / COUNT
+      name,
+      aian = aian / COUNT,
+      api = api / COUNT,
+      black = black / COUNT,
+      hispanic = hispanic / COUNT,
+      white = white / COUNT,
+      other = other / COUNT
     )
   
   p_surname_given_race <- race_surname_counts %>% 
     transmute (
-      NAME = NAME,
-      AIAN = AIAN / sum(race_surname_counts$AIAN),
-      API = API / sum(race_surname_counts$API),
-      BLACK = BLACK / sum(race_surname_counts$BLACK),
-      HISP = HISP / sum(race_surname_counts$HISP),
-      WHITE = WHITE / sum(race_surname_counts$WHITE),
-      OTHER = OTHER / sum(race_surname_counts$OTHER)
+      name,
+      aian = aian / sum(race_surname_counts$aian),
+      api = api / sum(race_surname_counts$api),
+      black = black / sum(race_surname_counts$black),
+      hispanic = hispanic / sum(race_surname_counts$hispanic),
+      white = white / sum(race_surname_counts$white),
+      other = other / sum(race_surname_counts$other)
     )
   
   write_rds(p_race_given_surname, paste0(DIR_PATH,"p_race_given_surname.rds"))
   write_rds(p_surname_given_race, paste0(DIR_PATH,"p_surname_given_race.rds"))
   
-} else {
-  
-  p_race_given_surname <- read_rds(paste0(DIR_PATH,"p_race_given_surname.rds"))
-  p_surname_given_race <- read_rds(paste0(DIR_PATH,"p_surname_given_race.rds"))
-  
 }
 
-cfips <- read_tsv(paste0(DIR_PATH,'county_list.tsv')) %>% 
-  mutate(countystr  = paste("state:",substr(FIPS,1,2),"+county:",substr(FIPS,3,5),sep="")) %>% 
-  pull(countystr)
-
-# Race and Location (CBG)
-if (!file.exists(paste0(DIR_PATH,"p_race_given_cbg.rds")) | !file.exists(paste0(DIR_PATH,"p_cbg_given_race.rds")) | 
-    !file.exists(paste0(DIR_PATH,"p_race.rds")) | !USE_CACHED) {
-  
-  raw_scc_race_cbg <- NULL
-  for (countystr in cfips) {
-    
-    # show(countystr)
-    temp <- tryCatch({
-      getCensus(
-        name = "acs/acs5",
-        vintage = 2018,
-        region = "block group:*",
-        regionin = countystr,
-        vars = "group(B03002)"
-      ) %>% return()
-    },
-    error = function(e) {
-      show("BAD CALL")
-      show(e)
-    })
-    
-    raw_scc_race_cbg <- rbind(raw_scc_race_cbg,temp)
-    
-  }
-  
-  acs_vars_2018_5yr <- listCensusMetadata(
-    name = "2018/acs/acs5",
-    type = "variables"
-  )
-  
-  scc_race_cbg <- raw_scc_race_cbg %>%   
-    mutate(CBG = paste0(state,county,tract,block_group)) %>%
-    select(!c(GEO_ID,state,county,tract,block_group,NAME) & 
-             !ends_with(c("EA","MA","M"))) %>%
-    pivot_longer(
-      ends_with("E"),
-      names_to = "variable",
-      values_to = "estimate"
-    ) %>%
-    left_join(acs_vars_2018_5yr %>% select(name, label), by = c("variable" = "name")) %>%
-    select(-variable) %>%
-    separate(
-      label,
-      into = c(NA,NA,"his.lat","category","subcategory"),
-      sep = "!!"
-    ) %>%
-    mutate(
-      race = case_when(
-        is.na(his.lat) ~ "TOTAL",
-        his.lat == "Hispanic or Latino" & is.na(category) ~ "HISP",
-        his.lat == "Hispanic or Latino" & !is.na(category) ~ "remove",
-        his.lat == "Not Hispanic or Latino" & 
-          category %in% c("Some other race alone", "Two or more races") ~ "remove",
-        his.lat == "Not Hispanic or Latino" & !is.na(category) ~ category,
-        his.lat == "Not Hispanic or Latino" & is.na(category) ~ "remove",
-        TRUE ~ subcategory
-      )
-    ) %>%
-    filter(race != "remove") %>%
-    select(CBG, race, estimate) %>% 
-    pivot_wider(names_from=race, values_from=estimate) %>% 
-    mutate(
-      OTHER = TOTAL - rowSums(.[3:8]),
-      API = `Asian alone` + `Native Hawaiian and Other Pacific Islander alone`
-    ) %>%
-    select(
-      CBG, 
-      TOTAL,
-      AIAN = `American Indian and Alaska Native alone`,
-      API,
-      BLACK = `Black or African American alone`,
-      HISP,
-      WHITE = `White alone`,
-      OTHER
-    ) %>% 
-    mutate(
-      AIAN = AIAN + LAPLACE,
-      API = API + LAPLACE,
-      BLACK = BLACK + LAPLACE,
-      HISP = HISP + LAPLACE,
-      WHITE = WHITE + LAPLACE,
-      OTHER = OTHER + LAPLACE,
-      TOTAL = TOTAL + 6 * LAPLACE
-    )
-  
-  p_race_given_cbg <- scc_race_cbg %>%
-    transmute (
-      CBG = CBG,
-      AIAN = AIAN / TOTAL,
-      API = API / TOTAL,
-      BLACK = BLACK / TOTAL,
-      HISP = HISP / TOTAL,
-      WHITE = WHITE / TOTAL,
-      OTHER = OTHER / TOTAL
-    ) %>% 
-    arrange(CBG)
-  
-  p_cbg <- scc_race_cbg %>% 
-    select(CBG, TOTAL) %>% 
-    transmute(
-      CBG = CBG,
-      p = TOTAL / sum(scc_race_cbg$TOTAL)
-    ) %>%
-    arrange(CBG)
-  
-  p_race <- 
-    sweep(data.matrix(p_race_given_cbg %>% select(-CBG)), 1, p_cbg$p, FUN="*") %>% 
-    colSums() %>% 
-    as_tibble_row()
-  
-  p_cbg_given_race <-
-    sweep(data.matrix(p_race_given_cbg %>% select(-CBG)), 1, p_cbg$p, FUN="*") %>% 
-    sweep(., 2, unlist(p_race), FUN="/") %>% 
-    as_tibble() %>% 
-    bind_cols(p_cbg %>% select(CBG), .)
-  
-  write_rds(p_race_given_cbg, paste0(DIR_PATH,"p_race_given_cbg.rds"))
-  write_rds(p_cbg_given_race, paste0(DIR_PATH,"p_cbg_given_race.rds"))
-  write_rds(p_race, paste0(DIR_PATH,"p_race.rds"))
-  
-} else {
-  
-  p_race_given_cbg <- read_rds(paste0(DIR_PATH,"p_race_given_cbg.rds"))
-  p_cbg_given_race <- read_rds(paste0(DIR_PATH,"p_cbg_given_race.rds"))
-  p_race <- read_rds(paste0(DIR_PATH,"p_race.rds"))
-  
-}
-
+# Firstname and Race
 if(!file.exists(paste0(DIR_PATH,"p_race_given_firstname.rds")) | !file.exists(paste0(DIR_PATH,"p_firstname_given_race.rds")) | !USE_CACHED) {
   
   first_names <- read_xlsx(paste0(DIR_PATH,"firstnames.xlsx"), sheet="Data") %>% 
@@ -218,46 +79,253 @@ if(!file.exists(paste0(DIR_PATH,"p_race_given_firstname.rds")) | !file.exists(pa
     filter(!startsWith(firstname, "ALL OTHER"))
   
   first_name_counts <- first_names %>% transmute (
-    NAME = firstname,
+    name = firstname,
     COUNT = obs,
-    AIAN = pctaian * COUNT / 100 + LAPLACE,
-    API = pctapi * COUNT / 100 + LAPLACE,
-    BLACK = pctblack * COUNT / 100 + LAPLACE,
-    HISP = pcthispanic * COUNT / 100 + LAPLACE,
-    WHITE = pctwhite * COUNT / 100 + LAPLACE,
-    OTHER = COUNT + 6 * LAPLACE - (AIAN + API + BLACK + HISP + WHITE)
-  ) %>% mutate (
-    COUNT = COUNT + 6 * LAPLACE
+    aian = pctaian * COUNT / 100,
+    api = pctapi * COUNT / 100,
+    black = pctblack * COUNT / 100,
+    hispanic = pcthispanic * COUNT / 100,
+    white = pctwhite * COUNT / 100,
+    other = COUNT - (aian + api + black + hispanic + white)
   )
   
   p_race_given_firstname <- first_name_counts %>% 
     transmute (
-      NAME = NAME,
-      AIAN = AIAN / COUNT,
-      API = API / COUNT,
-      BLACK = BLACK / COUNT,
-      HISP = HISP / COUNT,
-      WHITE = WHITE / COUNT,
-      OTHER = OTHER / COUNT
+      name,
+      aian = aian / COUNT,
+      api = api / COUNT,
+      black = black / COUNT,
+      hispanic = hispanic / COUNT,
+      white = white / COUNT,
+      other = other / COUNT
     )
   
   p_firstname_given_race <- first_name_counts %>% 
     transmute (
-      NAME = NAME,
-      AIAN = AIAN / sum(first_name_counts$AIAN),
-      API = API / sum(first_name_counts$API),
-      BLACK = BLACK / sum(first_name_counts$BLACK),
-      HISP = HISP / sum(first_name_counts$HISP),
-      WHITE = WHITE / sum(first_name_counts$WHITE),
-      OTHER = OTHER / sum(first_name_counts$OTHER),
+      name,
+      aian = aian / sum(first_name_counts$aian),
+      api = api / sum(first_name_counts$api),
+      black = black / sum(first_name_counts$black),
+      hispanic = hispanic / sum(first_name_counts$hispanic),
+      white = white / sum(first_name_counts$white),
+      other = other / sum(first_name_counts$other)
     )
   
   write_rds(p_race_given_firstname, paste0(DIR_PATH,"p_race_given_firstname.rds"))
   write_rds(p_firstname_given_race, paste0(DIR_PATH,"p_firstname_given_race.rds"))
   
-} else {
+}
+
+# Race and Location (CBG)
+if (!file.exists(paste0(DIR_PATH,"p_race_given_cbg.rds")) | !file.exists(paste0(DIR_PATH,"p_cbg_given_race.rds")) | 
+    !file.exists(paste0(DIR_PATH,"p_race.rds")) | !USE_CACHED) {
   
-  p_race_given_firstname <- read_rds(paste0(DIR_PATH,"p_race_given_firstname.rds"))
-  p_firstname_given_race <- read_rds(paste0(DIR_PATH,"p_firstname_given_race.rds"))
+  census_metadata <- 
+    listCensusMetadata(
+      name = paste0(census_year,"/acs/acs5"),
+      type = "variables"
+    )
+  
+  n_cores <- parallel::detectCores()
+  max_size = 1*1024^3
+  options(future.globals.maxSize= max_size)
+  plan(multisession, workers = n_cores / 2)
+  
+  # this helper function prepares a list of arguments for us to use
+  # before calling the census api given the geographic granularity we specify
+  prepare_census_query <- function(region, state, var, year){
+    
+    census_env <- list(
+      name = "acs/acs5",
+      vintage = year,
+      region = paste0(region, ":*"),
+      regionin = paste0("state:", state),
+      vars = c(var)
+    )
+    
+    if (region %in% c("us", "state", "zip code tabulation area")){
+      census_env$regionin <- NULL
+    }
+    
+    return(census_env)
+  }
+  
+  # this function downloads and processes the population data from census
+  # region: geographic granularity (tract, county, etc)
+  # state: two-digit state FIPS code to narrow the search
+  # var: variable name like group(B03002)
+  # group: asian or nhpi
+  download_census_data <- function(region, state, var, 
+                                   year=census_year){
+    
+    census_env <- prepare_census_query(region, state, var, year)
+    
+    # using non-standard evaluation here
+    census_raw <- tryCatch(rlang::exec(getCensus, !!!census_env), 
+                           error = function(e) NULL)
+    # get variable name and clean the format
+    if(is.null(census_raw)){
+      pop_df <- data.frame()
+    } else {
+      
+      pop_df <- 
+        census_raw %>% 
+        mutate(GEOID = str_remove(GEO_ID, ".*?US")) %>% 
+        select(GEOID, starts_with("B") & ends_with("E")) %>%
+        pivot_longer(
+          -GEOID,
+          names_to = "variable",
+          values_to = "estimate"
+        ) %>% 
+        left_join(
+          census_metadata %>% 
+            select(name, label), 
+          by = c("variable" = "name")
+        ) %>% 
+        mutate(
+          raceeth = case_when(
+            label == "Estimate!!Total:!!Not Hispanic or Latino:!!White alone" ~ "white",
+            label == "Estimate!!Total:!!Not Hispanic or Latino:!!Black or African American alone" ~ "black",
+            label == "Estimate!!Total:!!Not Hispanic or Latino:!!American Indian and Alaska Native alone" ~ "aian",
+            label == "Estimate!!Total:!!Not Hispanic or Latino:!!Asian alone" ~ "asian",
+            label == "Estimate!!Total:!!Not Hispanic or Latino:!!Native Hawaiian and Other Pacific Islander alone" ~ "nhpi",
+            label == "Estimate!!Total:!!Not Hispanic or Latino:!!Some other race alone" ~ "other",
+            label == "Estimate!!Total:!!Not Hispanic or Latino:!!Two or more races:" ~ "multi",
+            label == "Estimate!!Total:!!Hispanic or Latino:" ~ "hispanic"
+          )
+        ) %>% 
+        select(-label,-variable) %>% 
+        filter(!is.na(raceeth))
+      
+      if(nrow(pop_df) > 0){
+        pop_df <- pop_df %>% 
+          rename(pop = estimate) %>% 
+          pivot_wider(
+            names_from = "raceeth",
+            values_from = "pop"
+          ) %>% 
+          mutate(
+            api = asian + nhpi,
+            other = other + multi
+          ) %>% 
+          select(-asian,-nhpi,-multi) %>% 
+          filter(white + black + aian + other + hispanic + api > 0)
+      } else {
+        pop_df <- data.frame()
+      }
+      
+    }
+    
+    return(pop_df)
+  }
+  
+  # some states don't have tract data; exclude from loop
+  # "United States Virgin Islands"                
+  # "Commonwealth of the Northern Mariana Islands"
+  # "Guam"                                        
+  # "American Samoa"   
+  states <- states() %>% 
+    select(NAME,STATEFP) %>% 
+    filter(!STATEFP %in% c("78", "66", "60", "69"))
+  
+  cfips <- read_tsv(paste0(DIR_PATH,'county_list.tsv')) %>% 
+    mutate(countystr = paste(substr(FIPS,1,2),"+county:",substr(FIPS,3,5),sep="")) %>% 
+    pull(countystr)
+  
+  geo_result_1 <- future_map(cfips, function(statecounty){
+    geo_results <- 
+      download_census_data(state = statecounty, region = "block group",
+                           var = "group(B03002)",
+                           year = 2022)
+  })
+  
+  geo_result_1 <- geo_result_1 %>% 
+    bind_rows() %>% 
+    list()
+  
+  geo_result_2 <- future_map(c("tract", "county"), function(geo_level){
+    
+    geo_results_combined <- 
+      map_dfr(states$STATEFP, function(state){
+        geo_results <- 
+          download_census_data(state = state, region = geo_level,
+                               var = "group(B03002)",
+                               year = 2022)
+      })
+  })
+  
+  geo_result_3 <- future_map(c("state", "zip code tabulation area", "us"), 
+    function(geo_level){
+      
+    geo_results <- 
+      download_census_data(state = NULL, region = geo_level,
+                           var = "group(B03002)",
+                           year = 2022)
+    
+    if (geo_level == "us"){
+      geo_results <- geo_results %>% 
+        mutate(GEOID = "1")
+    }
+    
+    return(geo_results)
+  })
+  
+  geo_result_counts <- c(geo_result_1, geo_result_2, geo_result_3)
+  
+  saveRDS(
+    geo_result_counts, 
+    "geo_race_counts.rds"
+  )
+  
+  normalize_geo <- function(geo_df, geo_level){
+    
+    p_race_given_geo <- geo_df %>% 
+      mutate(count = rowSums(select(., -c(GEOID)))) %>% 
+      filter(count > 0) %>% 
+      mutate(
+        across(
+          -c(GEOID),
+          ~(./count)
+        )
+      ) %>% 
+      select(-count)
+    
+    p_geo_given_race <- geo_df %>% 
+      filter(GEOID %in% p_race_given_geo$GEOID) %>% 
+      mutate(
+        across(
+          -GEOID,
+          ~(./sum(.))
+        )
+      )
+    
+    if (geo_level == "zip code tabulation area"){
+      geo_level <- "zcta"
+    }
+    
+    geo_race <- paste("p", geo_level, "given_race", sep = "_")
+    race_geo <- paste("p_race_given", geo_level, sep = "_")
+    
+    geo_prior_list <- vector("list", length = 0)
+    
+    geo_prior_list[[geo_race]] <- p_geo_given_race
+    geo_prior_list[[race_geo]] <- p_race_given_geo
+    
+    return(geo_prior_list)
+    
+  }
+  
+  output_cond_tables <- 
+    map2(
+      geo_result_counts, 
+      c("cbg","tract","county","state","zip code tabulation area", "us"),
+      normalize_geo
+    ) %>% 
+    list_flatten()
+  
+  saveRDS(
+    output_cond_tables, 
+    paste0(DIR_PATH,"geo_race_table.rds")
+  )
   
 }
